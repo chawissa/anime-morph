4d1679625c8a1b1064da77e0d20295ca6fb6c5c8
Charissa Ramirez <chawissa@gmail.com>
2021-07-08 11:48:44 -0400

    Toggle dark mode

--- app.js
+++ app.js
@@ -13,8 +13,29 @@ darkMode.addEventListener("click", () => {
     easing: "easeOutExpo",
   });
   // Add different animations to the timeline
-  timeline.add({
-    targets: ".sun",
-    d: [{ value: moonPath }],
-  });
+  timeline
+    .add({
+      targets: ".sun",
+      d: [{ value: toggle ? sunPath : moonPath }],
+    })
+    .add(
+      {
+        targets: "#darkMode",
+        rotate: 320,
+      },
+      "-= 350"
+    )
+    .add(
+      {
+        targets: "section",
+        backgroundColor: toggle ? "rgb(255, 255, 255)" : "rgb(22, 22, 22)",
+        color: toggle ? "rgb(22, 22, 22" : "rgb(255, 255, 255)",
+      },
+      "-= 700"
+    );
+  if (!toggle) {
+    toggle = true;
+  } else {
+    toggle = false;
+  }
 });
